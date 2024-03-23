2d3d33f21c41bd8a6a9a04b4996a6ceb634906d4
julyangel977 <july@axistem.ru>
2024-03-23 19:24:43 +0700

    add all const

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import pygame
-
+import random
 pygame.init
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Kill Python")
-icon = pygame.image.load
+icon = pygame.image.load("Images/python.jpg")
+pygame.display.set_icon(icon)
+
+target_image = pygame.image.load(“Images/target_python.jpg”)
+target_width = 80
+target_height = 80
+target_x = random.randint(0, SCREEN_WIDTH - target_width)
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
 
 running = True
 while running:
